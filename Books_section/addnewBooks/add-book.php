b2f97cf043ee4e5b17a72d2e5daaec7432276579
PrabashaBAP <prabashapasindu672@gmail.com>
2026-02-16 22:02:57 +0530

    Fix stylesheet paths in add-book.php for correct loading

--- Books_section/addnewBooks/add-book.php
+++ Books_section/addnewBooks/add-book.php
@@ -4,8 +4,8 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Add New Book | OpenLib</title>
-  <link rel="stylesheet" href="../../assets/dashboard.css">
-  <link rel="stylesheet" href="../../assets/add-book.css">
+  <link rel="stylesheet" href="../assets/add-book.css">
+  <link rel="stylesheet" href="../assets/dashboard.css">
 </head>
 <body>
 
