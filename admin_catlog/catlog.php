5d34d66bf992fdc4de9dc75cb6462d34af8fcd18
PrabashaBAP <prabashapasindu672@gmail.com>
2026-02-17 12:36:28 +0530

    Update footer links in catalog page for correct file paths

--- admin_catlog/catlog.php
+++ admin_catlog/catlog.php
@@ -102,18 +102,18 @@
                 <div class="footer-column">
                     <h3>Quick Links</h3>
                     <ul>
-                        <li><a href="../dashboard/dashboard.html">Dashboard</a></li>    
-                        <li><a href="../Home/Main.html">Home</a></li>
-                        <li><a href="#">Book Catalog</a></li>
-                        <li><a href="../contact us/contact.html">Contact</a></li>
-                        <li><a href="../Abou_us/about_us.html">About Us</a></li>
+                        <li><a href="../Admin_dashboard/dashboard.php">Dashboard</a></li>    
+                        <li><a href="../Home/index.php">Home</a></li>
+                        <li><a href="../admin_catlog/catlog.php">Book Catalog</a></li>
+                        <li><a href="../contact/contact.php">Contact</a></li>
+                        <li><a href="../About us/About us.php">About Us</a></li>
                     </ul>
                 </div>
                 <div class="footer-column">
                     <h3>Account</h3>
                     <ul>
-                        <li><a href="../Login page/user_login.html">Log In</a></li>
-                        <li><a href="../Register/user_register.html">Sign Up</a></li>
+                        <li><a href="../Login/user_login.php">Log In</a></li>
+                        <li><a href="../Register/register.php">Sign Up</a></li>
                     </ul>
                 </div>
                 <div class="footer-column">
